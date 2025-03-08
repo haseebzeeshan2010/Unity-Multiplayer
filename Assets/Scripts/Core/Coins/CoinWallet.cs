3c33d635b07e8aa84eb9879cf06204b4819117dd
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2025-03-08 21:30:14 +0000

    Add Coin Spawner and Coin wallet

--- Assets/Scripts/Core/Coins/CoinWallet.cs
+++ Assets/Scripts/Core/Coins/CoinWallet.cs
@@ -1,16 +1,16 @@
 using UnityEngine;
-
+using Unity.Netcode;
 public class CoinWallet : NetworkBehaviour
 {
     public NetworkVariable<int> TotalCoins = new NetworkVariable<int>(0);
 
 
-    onTriggerEnter2D(Collider2D col)
+    private void OnTriggerEnter2D(Collider2D col)
     {
-        if (!col.TryGetComponent(out Coin coin)) {return;}
+        if (!col.TryGetComponent(out Coin coin)) { return; }
 
-        int coinValue = coin.Value;
-        if(!IsServer) {return;}
+        int coinValue = coin.Collect();
+        if(!IsServer) { return; }
 
         TotalCoins.Value += coinValue;
     }
