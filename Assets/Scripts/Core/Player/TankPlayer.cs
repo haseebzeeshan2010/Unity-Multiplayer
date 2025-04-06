040c6347db55f21fd491a1ad1fd8cb8f18ee6a2c
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2025-04-06 11:46:50 +0100

    Add player respawning

--- Assets/Scripts/Core/Player/TankPlayer.cs
+++ Assets/Scripts/Core/Player/TankPlayer.cs
@@ -2,16 +2,21 @@ using Unity.Netcode;
 using UnityEngine;
 using Unity.Cinemachine;
 using Unity.Collections;
+using System;
 
 public class TankPlayer : NetworkBehaviour
 {
     [Header("References")]
     [SerializeField] private CinemachineCamera virtualCamera;
+    [field: SerializeField] public Health Health { get; private set; } // Reference to the health component
 
     [Header("Settings")]
     [SerializeField] private int ownerPriority = 15;
 
     public NetworkVariable<FixedString32Bytes> PlayerName = new NetworkVariable<FixedString32Bytes>();
+
+    public static event Action<TankPlayer> OnPlayerSpawned; // Invoked when a player spawns
+    public static event Action<TankPlayer> OnPlayerDespawned; // Invoked when a player despawns
 
     public override void OnNetworkSpawn()
     {
@@ -21,6 +26,8 @@ public class TankPlayer : NetworkBehaviour
                 HostSingleton.Instance.GameManager.NetworkServer.GetUserDataByClientId(OwnerClientId);
             
             PlayerName.Value = userData.username;
+
+            OnPlayerSpawned?.Invoke(this); // Broadcast the onplayerspawned event when player spawns
         }
 
 
@@ -29,4 +36,13 @@ public class TankPlayer : NetworkBehaviour
             virtualCamera.Priority = ownerPriority;
         }
     }
+
+    public override void OnNetworkDespawn()
+    {
+        if(IsServer)
+        {
+            OnPlayerDespawned?.Invoke(this); // Broadcast the onplayerdespawned event when player despawns
+        }
+    }
+
 }
