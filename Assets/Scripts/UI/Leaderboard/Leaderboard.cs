ac69ab418c01991d5ebabe0e2d1cf1de0b46d249
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2025-04-10 17:06:11 +0100

    Add clients to the leaderboard on join

--- Assets/Scripts/UI/Leaderboard/Leaderboard.cs
+++ Assets/Scripts/UI/Leaderboard/Leaderboard.cs
@@ -11,4 +11,78 @@ public class Leaderboard : NetworkBehaviour
     {
         leaderboardEntities = new NetworkList<LeaderboardEntityState>();
     }
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsClient){
+            leaderboardEntities.OnListChanged += HandleLeaderBoardEntityListChanged;
+            foreach(LeaderboardEntityState entity in leaderboardEntities)
+            {
+                HandleLeaderBoardEntityListChanged(new NetworkListEvent<LeaderboardEntityState>
+                {
+                    Type = NetworkListEvent<LeaderboardEntityState>.EventType.Add,
+                    Value = entity
+                });
+                
+            }
+        }
+
+        if (IsServer) {
+            TankPlayer[] players = FindObjectsByType<TankPlayer>(FindObjectsSortMode.None);
+
+            foreach (TankPlayer player in players)
+            {
+                HandlePlayerSpawned(player);
+            }
+            TankPlayer.OnPlayerSpawned += HandlePlayerSpawned;
+            TankPlayer.OnPlayerDespawned += HandlePlayerDespawned;
+        }
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        if (IsClient){
+            leaderboardEntities.OnListChanged -= HandleLeaderBoardEntityListChanged;
+        }
+        if (IsServer) {
+            TankPlayer.OnPlayerSpawned -= HandlePlayerSpawned;
+            TankPlayer.OnPlayerDespawned -= HandlePlayerDespawned;
+        }
+    }
+
+    private void HandleLeaderBoardEntityListChanged(NetworkListEvent<LeaderboardEntityState> changeEvent)
+    {
+        switch(changeEvent.Type)
+        {
+            case NetworkListEvent<LeaderboardEntityState>.EventType.Add:
+                Instantiate(leaderboardEntityPrefab, laederboardEntityHolder);
+                break;
+            
+        }
+    }
+
+    private void HandlePlayerSpawned(TankPlayer player)
+    {
+        leaderboardEntities.Add(new LeaderboardEntityState
+        {
+            ClientId = player.OwnerClientId,
+            PlayerName = player.PlayerName.Value,
+            Coins = 0
+            
+        });
+    }
+
+    private void HandlePlayerDespawned(TankPlayer player)
+    {
+        if (leaderboardEntities == null) { return; }
+
+        foreach(LeaderboardEntityState entity in leaderboardEntities)
+        {
+            if(entity.ClientId != player.OwnerClientId){ continue; }
+
+            leaderboardEntities.Remove(entity);
+            break;
+
+        }
+    }
 }
